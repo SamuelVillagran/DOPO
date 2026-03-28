45883381ff4c9c225e75341937ebfeefebe7c7f7
Samuel Villagran <samuel.villagranr@gmail.com>
2026-03-27 19:45:03 -0500

    Agregando interfaz

---
+++ Talleres/Pre-parcial/CodigoPreParcial/Trustable.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of interface Trustable here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+public interface Trustable {
+    
+    boolean isReliable();
+    
+}
