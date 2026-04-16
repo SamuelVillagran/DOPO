dae5fd972ece1afd7556388cde2c398b34c3f066
HDavid Sánchez <hsanchezalarcon@gmail.com>
2026-04-16 17:29:25 -0500

    sokobanGUI inicial

--- LABS/LAB05/EasySokoban/src/presentation/SokobanGUI.java
+++ LABS/LAB05/EasySokoban/src/presentation/SokobanGUI.java
@@ -1,19 +1,29 @@
 package presentation;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.*;
 
 
 public class SokobanGUI extends JFrame{
 	
 	public SokobanGUI() {
-		super("EasySOkoban");
-		setSize(300, 400);
-		setLocation(500, 500);	
+		prepareElements();
+		
+	}
+	
+	private void prepareElements() {
+		setTitle("EasySokoban");
+		Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+		int width = (int)size.getWidth() / 2;
+		int height = (int)size.getHeight() / 2;
+		setSize(width, height);
+		setLocationRelativeTo(null);
 		setVisible(true);
 	}
 	
-	
 	public static void main (String[] args) {
 		SokobanGUI sokobanGUI = new SokobanGUI();
-		
+		sokobanGUI.setVisible(true);
 	}
 }
