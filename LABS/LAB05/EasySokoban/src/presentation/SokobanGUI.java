64b0a8513dbcf90f8c32ff100e8dae1298545612
HDavid Sánchez <hsanchezalarcon@gmail.com>
2026-04-16 23:58:24 -0500

    confirmacion Salir

--- LABS/LAB05/EasySokoban/src/presentation/SokobanGUI.java
+++ LABS/LAB05/EasySokoban/src/presentation/SokobanGUI.java
@@ -1,14 +1,17 @@
 package presentation;
+
 import java.awt.Dimension;
 import java.awt.Toolkit;
-
+import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
 
 public class SokobanGUI extends JFrame{
 	
 	public SokobanGUI() {
 		prepareElements();
+		prepareActions();
 		
 	}
 	
@@ -20,7 +23,22 @@ public class SokobanGUI extends JFrame{
 		setSize(width, height);
 		setLocationRelativeTo(null);
 		setVisible(true);
+		
 	}
+	
+	
+	private void prepareActions() {
+		setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				int result = JOptionPane.showConfirmDialog(SokobanGUI.this, "¿Está seguro que desea salir?");
+				switch(result) {
+					case JOptionPane.YES_OPTION -> System.exit(0);
+				}
+			}
+		});
+	}
+	
 	
 	public static void main (String[] args) {
 		SokobanGUI sokobanGUI = new SokobanGUI();
