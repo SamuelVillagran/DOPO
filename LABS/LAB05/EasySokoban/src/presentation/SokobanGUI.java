b64e2d826092380425a43a4a3f61536661a8f65e
HDavid Sánchez <hsanchezalarcon@gmail.com>
2026-04-25 17:50:14 -0500

    Forma ventana principal

--- LABS/LAB05/EasySokoban/src/presentation/SokobanGUI.java
+++ LABS/LAB05/EasySokoban/src/presentation/SokobanGUI.java
@@ -1,7 +1,12 @@
  package presentation;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
-
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -17,14 +22,23 @@ public class SokobanGUI extends JFrame{
 	private JMenu menuFile;
 	private JMenuItem menuItemNew, menuItemOpen, menuItemSave, menuItemExit;
 	
+	//Atributos board
+	private JTextField score, time, configHeight, configWidth;
+	private JPanel infoPanel, optionsPanel, configPanel, controlPanel, arrowsPanel, boardPanel;
+	private JButton btnPlay, btnChangeColor, btnRefresh, btnDown, btnRight, btnLeft, btnUp;
+	private JPanel[] cells;
+	
+	
 	
 	public SokobanGUI() {
+		super("EasySokoban");
 		prepareElements(); /*Vista*/
 		prepareActions();  /*Controlador*/
+		prepareElementsBoard();
+		prepareActionsBoard();
 	}
 
 	private void prepareElements() {
-		setTitle("EasySokoban");
 		setScreen();
 		elementsPanelButtons();
 		elementsPanelImage();
@@ -92,6 +106,107 @@ public class SokobanGUI extends JFrame{
 		
 	}
 
+	private void prepareElementsBoard() {
+		//Asignando el layout como BorderLayout
+		setLayout(new BorderLayout());
+		
+		
+		//Configuracion zona North
+		configPanel = new JPanel(new FlowLayout());
+		configPanel.add(new JLabel("Height:"));
+		configHeight = new JTextField(3);
+		configPanel.add(configHeight);
+		
+		configPanel.add(new JLabel("Width"));
+		configWidth = new JTextField(3);
+		configPanel.add(configWidth);
+		
+		btnPlay = new JButton("Play");
+		
+		configPanel.add(btnPlay);
+		
+		add(configPanel, BorderLayout.NORTH);
+		
+		
+		//Configuracion zona West
+		optionsPanel = new JPanel();
+		optionsPanel.setLayout(new GridLayout(4,1, 10, 10));
+		optionsPanel.add(new JButton("Modify"));
+		optionsPanel.add(new JButton("Save"));
+		optionsPanel.add(new JButton("Restart"));
+		optionsPanel.add(new JButton("Open"));
+		
+		add(optionsPanel, BorderLayout.WEST);
+		
+		
+		//Configuracion botones del juego.
+		
+		arrowsPanel = new JPanel(new BorderLayout());
+		controlPanel = new JPanel(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.insets = new Insets(4,4,4,4);
+		
+		btnLeft = new JButton("w");
+		c.gridx = 0;
+		c.gridy = 1;
+		controlPanel.add(btnLeft, c);
+		
+		btnRight = new JButton("R");
+		c.gridx = 3;
+		c.gridy = 1;
+		controlPanel.add(btnRight,c);
+		
+		btnDown = new JButton("D");
+		c.gridx = 1;
+		c.gridy = 2;
+		c.gridwidth = 2;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		controlPanel.add(btnDown, c);
+		
+		btnUp = new JButton("Up");
+		c.gridx = 2;
+		c.gridy = 1;
+		c.gridwidth = 1;
+		controlPanel.add(btnUp, c);
+		
+		arrowsPanel.add(controlPanel, BorderLayout.CENTER);
+		add(arrowsPanel, BorderLayout.SOUTH);
+		
+		
+		//Zona East Zone
+		
+		//Info panel
+		JPanel eastPanel = new JPanel(new BorderLayout());
+		infoPanel = new JPanel(new GridLayout(2,1,5,5));
+		infoPanel.add(new JLabel("Score:"));
+		JTextField infoScore = new JTextField("0", 5);
+		infoScore.setEditable(false);
+		infoPanel.add(infoScore);
+		
+		infoPanel.add(new JLabel("Time:"));
+		JTextField infoTime = new JTextField("00:00", 5);
+		infoTime.setEditable(false);
+		infoPanel.add(infoTime);
+		
+		//Opciones panel
+		optionsPanel = new JPanel(new GridLayout(2,1,10,15));
+		btnChangeColor = new JButton("Change Color");
+		optionsPanel.add(btnChangeColor);
+		btnRefresh = new JButton("Refresh");
+		optionsPanel.add(btnRefresh);
+		
+		eastPanel.add(infoPanel, BorderLayout.NORTH);
+		eastPanel.add(optionsPanel, BorderLayout.CENTER);
+		
+		add(eastPanel, BorderLayout.EAST);
+		
+		setVisible(true);
+		
+	}
+	
+	private void prepareActionsBoard() {
+		
+	}
 	private void setScreen() {
 		Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
 		int width = (int)size.getWidth() / 2;
@@ -101,18 +216,6 @@ public class SokobanGUI extends JFrame{
 		setVisible(true);
 	}
 	
-	
-	private void prepareActions() {
-		setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
-		addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent e) {
-				int result = JOptionPane.showConfirmDialog(SokobanGUI.this, "¿Está seguro que desea salir?");
-				switch(result) {
-					case JOptionPane.YES_OPTION -> System.exit(0);
-				}
-			}
-		});
-	}
 	
 	public static void main (String[] args) {
 		SokobanGUI gui = new SokobanGUI();
