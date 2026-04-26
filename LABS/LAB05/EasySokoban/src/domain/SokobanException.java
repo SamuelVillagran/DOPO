9a373404d748ff7e550f762de711856b63a568d1
HDavid Sánchez <hsanchezalarcon@gmail.com>
2026-04-25 23:25:54 -0500

    excepcion nueva

--- LABS/LAB05/EasySokoban/src/domain/SokobanException.java
+++ LABS/LAB05/EasySokoban/src/domain/SokobanException.java
@@ -1,9 +1,14 @@
 package domain;
 
+/**
+ * Exceptions class own Sokoban Game.
+ */
 public class SokobanException extends Exception {
 
 	public static final String PLAYER_CANT_MOVE = "Player try moves to some invalid moves.";
 	public static final String BOARD_TOO_SMALL = "Height or width aren't correctly, try with others dimensions.";
+	public static final String BOARD_TOO_BIG= "Tablero demasiado grande! Reduzca su tamaño.";
+	public static final String HEIGTH_OR_WIDTH_INVALID = "Parametros de altura o ancho inválidos.";
 	
 	public SokobanException(String message) {
 		super(message);
