7879e32cb7b1b2554756bda5ecdae3aae41f6d27
HDavid Sánchez <hsanchezalarcon@gmail.com>
2026-04-30 16:41:00 -0500

    Cambiando excepciones encapsuladas

--- LABS/LAB06/forest/src/domain/ForestException.java
+++ LABS/LAB06/forest/src/domain/ForestException.java
@@ -5,4 +5,8 @@ public class ForestException extends Exception{
 	public ForestException(String message) {
 		super(message);
 	}
+	
+	public ForestException(String operation, String file) {
+		super("Operación " + operation + " en construcción. Archivo " + file);
+	}
 }
